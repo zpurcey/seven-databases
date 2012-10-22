8d6193dcabe735e7771a8055492b4a5f1491d83c
Craig Aspinall <craig.aspinall@gmail.com>
2012-10-22 23:12:38 +1000

    Converted to Neo4J Enterprise Edition.

--- 7-neo4j/provision.sh
+++ 7-neo4j/provision.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ ! -d /home/vagrant/neo4j-community-1.8 ]
+if [ ! -d /home/vagrant/neo4j-enterprise-1.8 ]
 then
   echo "Installing pre-requisites"
   apt-get -qq update
@@ -10,12 +10,12 @@ then
   echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/" >> /home/vagrant/.profile
 
   echo "Installing Neo4J"
-  tar xfz /vagrant/neo4j-community-1.8-unix.tar.gz
-  chown -hR vagrant.vagrant neo4j-community-1.8
+  tar xfz /vagrant/neo4j-enterprise-1.8-unix.tar.gz
+  chown -hR vagrant.vagrant neo4j-enterprise-1.8
 
-  echo "export NEO4J_HOME=/home/vagrant/neo4j-community-1.8" >> /home/vagrant/.profile
-  echo "export PATH=$PATH:$NEO4J_HOME/bin" >> /home/vagrant/.profile
-  sed -e '17 s/#//' -i /home/vagrant/neo4j-community-1.8/conf/neo4j-server.properties
+  echo "export NEO4J_HOME=/home/vagrant/neo4j-enterprise-1.8" >> /home/vagrant/.profile
+  echo "export PATH=$PATH:${NEO4J_HOME}/bin" >> /home/vagrant/.profile
+  sed -e '17 s/#//' -i /home/vagrant/neo4j-enterprise-1.8/conf/neo4j-server.properties
   sed -e '2 s/127.0.1.1/127.0.0.1/' -i /etc/hosts
 
   echo "Done!"
